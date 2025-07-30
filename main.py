9b0d9b68f4fa0b5275becc760516a41b5e539096
vezovius <aidinmahdavi@gmail.com>
2025-07-30 19:41:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,38 +2,30 @@ import os
 import telebot
 from flask import Flask, request
 
-TOKEN = os.getenv("BOT_TOKEN", "7776065669:AAEWGA0PI1Hs0NwuNTRQUGU8Atkeuhbetdg")
+TOKEN = os.getenv("7776065669:AAEWGA0PI1Hs0NwuNTRQUGU8Atkeuhbetdg")
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-# دستور شروع
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+json_str = request.get_data().decode('UTF-8')
+update = telebot.types.Update.de_json(json_str)
+bot.process_new_updates([update])
+return "OK", 200
+
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.reply_to(message, "Merhaba! Hayvan türünü giriniz.")
+def start(message):
+bot.reply_to(message, "Merhaba! Hayvan türünü giriniz.")
 
-# دریافت متن
 @bot.message_handler(func=lambda message: True)
-def save_message(message):
-    bot.reply_to(message, f"Teşekkürler! '{message.text}' kaydedildi.")
+def echo_all(message):
+bot.reply_to(message, "Teşekkürler! Mesajınız kaydedildi.")
 
-# مسیر وبهوک
-@app.route(f'/{TOKEN}', methods=['POST'])
+@app.route("/")
 def webhook():
-    json_str = request.get_data().decode('UTF-8')
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return '', 200
-
-# مسیر اصلی برای تست
-@app.route('/', methods=['GET'])
-def index():
-    return "Bot is running!", 200
+bot.remove_webhook()
+bot.set_webhook(url=os.environ.get("RENDER_EXTERNAL_URL") + "/" + TOKEN)
+return "Webhook set", 200
 
 if __name__ == "__main__":
-    # ست کردن وبهوک به صورت خودکار
-    RENDER_URL = os.getenv("RENDER_EXTERNAL_URL")
-    if RENDER_URL:
-        bot.remove_webhook()
-        bot.set_webhook(url=f"{RENDER_URL}/{TOKEN}")
-
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
